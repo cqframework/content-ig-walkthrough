4905bd52e0af4b319a5f5b8bfe67844ad62eef8b
Rob Reynolds <grobreynolds@gmail.com>
2022-09-28 15:01:03 -0600

    Update _updatePublisher.sh
    Comment updating scripts as it stomps the pin of the publisher.

--- _updatePublisher.sh
+++ _updatePublisher.sh
@@ -104,30 +104,30 @@ if [[ $skipPrompts != true ]]; then
     read -r -p "$message" response
   fi
 
-if [[ $skipPrompts == true ]] || [[ $response =~ ^[yY].*$ ]]; then
-  echo "Downloading most recent scripts "
+# if [[ $skipPrompts == true ]] || [[ $response =~ ^[yY].*$ ]]; then
+#  echo "Downloading most recent scripts "
 
-  curl -L $update_bat_url -o /tmp/_updatePublisher.new
-  cp /tmp/_updatePublisher.new _updatePublisher.bat
-  rm /tmp/_updatePublisher.new
+#  curl -L $update_bat_url -o /tmp/_updatePublisher.new
+#  cp /tmp/_updatePublisher.new _updatePublisher.bat
+#  rm /tmp/_updatePublisher.new
 
-  curl -L $gen_bat_url -o /tmp/_genonce.new
-  cp /tmp/_genonce.new _genonce.bat
-  rm /tmp/_genonce.new
+#  curl -L $gen_bat_url -o /tmp/_genonce.new
+#  cp /tmp/_genonce.new _genonce.bat
+#  rm /tmp/_genonce.new
 
-  curl -L $gencont_bat_url -o /tmp/_gencontinuous.new
-  cp /tmp/_gencontinuous.new _gencontinuous.bat
-  rm /tmp/_gencontinuous.new
+#  curl -L $gencont_bat_url -o /tmp/_gencontinuous.new
+#  cp /tmp/_gencontinuous.new _gencontinuous.bat
+#  rm /tmp/_gencontinuous.new
 
-  curl -L $gencont_sh_url -o /tmp/_gencontinuous.new
-  cp /tmp/_gencontinuous.new _gencontinuous.sh
-  rm /tmp/_gencontinuous.new
+#  curl -L $gencont_sh_url -o /tmp/_gencontinuous.new
+#  cp /tmp/_gencontinuous.new _gencontinuous.sh
+#  rm /tmp/_gencontinuous.new
 
-  curl -L $gen_sh_url -o /tmp/_genonce.new
-  cp /tmp/_genonce.new _genonce.sh
-  rm  /tmp/_genonce.new
+#  curl -L $gen_sh_url -o /tmp/_genonce.new
+#  cp /tmp/_genonce.new _genonce.sh
+#  rm  /tmp/_genonce.new
 
-  curl -L $update_sh_url -o /tmp/_updatePublisher.new
-  cp /tmp/_updatePublisher.new _updatePublisher.sh
-  rm /tmp/_updatePublisher.new
-fi
+#  curl -L $update_sh_url -o /tmp/_updatePublisher.new
+#  cp /tmp/_updatePublisher.new _updatePublisher.sh
+#  rm /tmp/_updatePublisher.new
+# fi
